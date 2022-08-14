7214d3b9a80eea24f967ba6c9e2ae72cb81358ef
Valeriy Stavikiy <v.s@leadsdone.net>
2022-08-14 14:24:05 +0300

    add 8 func

---
+++ 8_functions.py
@@ -0,0 +1,31 @@
+# def print_value():
+#     print(2+2)
+#
+#
+# print(print_value)
+#
+# def func(a):
+#     print(a*70)
+#
+#
+# func(10)
+
+# def func2(a, b, c):
+#     print(a + b + c)
+#
+#
+# func2(10)
+
+print('--------------')
+def func_sum(a, b):
+    return a + b
+
+
+result = func_sum(2, 5)
+print(result)
+result2 = func_sum(7, 42)
+print(result2)
+
+
+
+
---
+++ 8_functions_task.py
@@ -0,0 +1,30 @@
+# Найдите максимальное значение в передаваемом в функцию списке и верните его,
+# если оно больше 0б в ином случае верните сообщение о том, что число меньше 0.
+def max_val(arr):
+    result = arr[0]
+    for num in arr:
+        if num > result:
+            result = num
+
+    return result if result > 0 else 'в списке нет чисел больше нуля'
+
+
+print(max_val([1, 5, 8, 7, 89, 7, 0]))
+print(max_val([-1, -5, -8, -7, -89, -7, 0]))
+print('--------------')
+
+
+def count_str(str):
+    return len(str)
+
+
+print(count_str("Пырышки пупырышки"))
+print('--------------')
+
+
+def power(num, step):
+    return num ** step if step > 0 else None
+
+
+print(power(2, 4))
+print(power(2, -4))
